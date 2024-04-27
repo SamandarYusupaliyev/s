47af7467660d9c1eed31f0ffc04781b80676f4cd
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-28 01:53:19 +0500

    papka

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -11,9 +11,29 @@ export const productsSlice =createSlice({
   name:"products",
   initialState,
   reducers:{
-    increaseAmount:(state,{payload})=>{},
-    decreaseAmount:(state,{payload})=>{},
+    increaseAmount:(state,{payload})=>{
+      const item = state.products.find((product)=>{
+        return product.id == payload;
+      });
+      item.amount +=1;
+    },
+    decreaseAmount:(state,{payload})=>{
+      const item = state.products.find((product)=>{
+        return product.id == payload;
+      });
+      item.amount -=1;
+    },
     removeItem:(state,{payload})=>{},
+    calulateTotal:(state)=>{
+      let price =0;
+      let total =0;
+      state.products.forEach((product)=>{
+        total= total+product.amount;
+        price = price+product.amount*product.price;
+      })
+      state .price =price;
+      state.total =total;
+    },
   },
 })
 
