799439d8f6f374afc7a7332fcd0809e6dc7c2066
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-29 09:09:33 +0500

    papka

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import Navbar from "./components/Navbar"
+import { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { calulateTotal } from './features/products/productSlice'
+import { useDispatch } from 'react-redux'
+import ProductsList from './components/ProductsList'
 
 function App() {
-    return(
-        <div className="max-w-4xl mx-auto">
-            <Navbar/>
-        </div>
-    )
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calulateTotal())
+
+  },[])
+  return (
+    <div className='max-w-4xl mx-auto mb-10 '>
+      <Navbar/>
+      <ProductsList/>
+     
+    </div>
+  )
 }
 
 export default App
